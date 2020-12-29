6ed94301fee6fb62492fb3474c33c2d3ad24919d
buinopia <denisdemchenko007@gmail.com>
2020-12-29 19:38:11 +0200

    added gitignore

---
+++ readme.md
@@ -0,0 +1,1 @@
+# Added Value for Html file
---
+++ script.js
@@ -0,0 +1,8 @@
+var y = 0;
+
+function Click() {
+    var num;
+    num = document.getElementById('num3').value;
+    y += 1
+    document.getElementById('num3').innerHTML = y;
+}
